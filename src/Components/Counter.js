245aea29f667c2b9800bc95c345346bf82b156f5
munenakoppa <30587898+munenakoppa@users.noreply.github.com>
2024-05-02 08:28:04 +0530

    Add files via upload

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,1 +1,45 @@
+import { useState } from "react";
+import "./Counter.css";
 
+function Counter() {
+  //let count = 0;
+  let [count, setCount] = useState(0);
+  function incrementCount() {
+    if (count !== 15) setCount(count + 1);
+  }
+  function decrementCount() {
+    if (count !== 0) setCount(count - 1);
+  }
+  function clearCount() {
+    setCount(0);
+  }
+  return (
+    <div>
+      <h1>Counter Component by Button</h1>
+      <button
+        className="button"
+        onClick={incrementCount}
+        title="Increase the count"
+      >
+        +
+      </button>
+      <button class="button button4">{count}</button>
+      <button 
+        className="button button3"
+        onClick={decrementCount}
+        title="Decrease the count"
+      >
+        -
+      </button>
+      <button
+        className="button button2"
+        onClick={clearCount}
+        title="Clear the count"
+      >
+        x
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
